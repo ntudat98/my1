deafc343caa1583128c5329d74ccb4493e6d4303
ntudat <nguyentudat19989@gmail.com>
2023-08-21 17:47:50 +0700

    init

---
+++ my1/src/main/java/com/example/sample/My1Application.java
@@ -0,0 +1,12 @@
+package myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class My1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(My1Application.class, args);
+  }
+}
